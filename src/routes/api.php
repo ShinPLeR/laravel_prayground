164d26effa7a56b9a99ce346a3e929426a4ca688
S.I <diggy_world@yahoo.co.jp>
2023-09-02 21:14:55 +0900

    add redis container

--- src/routes/api.php
+++ src/routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('set', [\App\Http\Controllers\Api\SessionController::class, 'set']);
+Route::get('get', [\App\Http\Controllers\Api\SessionController::class, 'get']);
